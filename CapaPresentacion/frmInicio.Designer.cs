8dd9e6c01f67624638f7d40a02ae4332681e9718
Miguel <miguel-marzo@hotmail.es>
2017-01-18 12:23:41 +0100

    Created frmInicio and template for the layout of the buttons

---
+++ CapaPresentacion/frmInicio.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecogida = new System.Windows.Forms.Button();
+            this.btnRegistro = new System.Windows.Forms.Button();
+            this.btnVenta = new System.Windows.Forms.Button();
+            this.btnDevolucion = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRecogida
+            // 
+            this.btnRecogida.Font = new System.Drawing.Font("Palatino Linotype", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecogida.Location = new System.Drawing.Point(49, 46);
+            this.btnRecogida.Name = "btnRecogida";
+            this.btnRecogida.Size = new System.Drawing.Size(271, 149);
+            this.btnRecogida.TabIndex = 0;
+            this.btnRecogida.Text = "Recogida";
+            this.btnRecogida.UseVisualStyleBackColor = true;
+            // 
+            // btnRegistro
+            // 
+            this.btnRegistro.Font = new System.Drawing.Font("Palatino Linotype", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistro.Location = new System.Drawing.Point(410, 46);
+            this.btnRegistro.Name = "btnRegistro";
+            this.btnRegistro.Size = new System.Drawing.Size(271, 149);
+            this.btnRegistro.TabIndex = 1;
+            this.btnRegistro.Text = "Registro";
+            this.btnRegistro.UseVisualStyleBackColor = true;
+            // 
+            // btnVenta
+            // 
+            this.btnVenta.Font = new System.Drawing.Font("Palatino Linotype", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVenta.Location = new System.Drawing.Point(49, 253);
+            this.btnVenta.Name = "btnVenta";
+            this.btnVenta.Size = new System.Drawing.Size(271, 149);
+            this.btnVenta.TabIndex = 2;
+            this.btnVenta.Text = "Venta";
+            this.btnVenta.UseVisualStyleBackColor = true;
+            // 
+            // btnDevolucion
+            // 
+            this.btnDevolucion.Font = new System.Drawing.Font("Palatino Linotype", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDevolucion.Location = new System.Drawing.Point(410, 253);
+            this.btnDevolucion.Name = "btnDevolucion";
+            this.btnDevolucion.Size = new System.Drawing.Size(271, 149);
+            this.btnDevolucion.TabIndex = 3;
+            this.btnDevolucion.Text = "Devolución";
+            this.btnDevolucion.UseVisualStyleBackColor = true;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 485);
+            this.Controls.Add(this.btnDevolucion);
+            this.Controls.Add(this.btnVenta);
+            this.Controls.Add(this.btnRegistro);
+            this.Controls.Add(this.btnRecogida);
+            this.Name = "frmInicio";
+            this.Text = "CuaShop";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRecogida;
+        private System.Windows.Forms.Button btnRegistro;
+        private System.Windows.Forms.Button btnVenta;
+        private System.Windows.Forms.Button btnDevolucion;
+    }
+}
+
