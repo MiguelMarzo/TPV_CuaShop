6ba5f5f5a42b98aa00ae53ebad5110b8cd6be124
ezkerro <davidandola63@gmail.com>
2017-01-25 11:34:29 +0100

    fmrRecogida

---
+++ CapaPresentacion/frmRecogida.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmRecogida
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbEmpleado = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblNumeroRecogida = new System.Windows.Forms.Label();
+            this.btnVolver = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbEntregador = new System.Windows.Forms.ComboBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lblCantidadArticulos = new System.Windows.Forms.Label();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(408, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(373, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RECOGIDA DE PRODUCTOS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(879, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Fecha:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(879, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(132, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nº Empleado:";
+            // 
+            // cmbEmpleado
+            // 
+            this.cmbEmpleado.FormattingEnabled = true;
+            this.cmbEmpleado.Location = new System.Drawing.Point(1023, 26);
+            this.cmbEmpleado.Name = "cmbEmpleado";
+            this.cmbEmpleado.Size = new System.Drawing.Size(133, 21);
+            this.cmbEmpleado.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(39, 126);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 233);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(854, 126);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(308, 233);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(389, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 21);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Número de recogida:";
+            // 
+            // lblNumeroRecogida
+            // 
+            this.lblNumeroRecogida.AutoSize = true;
+            this.lblNumeroRecogida.Location = new System.Drawing.Point(626, 132);
+            this.lblNumeroRecogida.Name = "lblNumeroRecogida";
+            this.lblNumeroRecogida.Size = new System.Drawing.Size(0, 13);
+            this.lblNumeroRecogida.TabIndex = 7;
+            // 
+            // btnVolver
+            // 
+            this.btnVolver.Location = new System.Drawing.Point(94, 28);
+            this.btnVolver.Name = "btnVolver";
+            this.btnVolver.Size = new System.Drawing.Size(153, 56);
+            this.btnVolver.TabIndex = 8;
+            this.btnVolver.Text = "VOLVER";
+            this.btnVolver.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(389, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(122, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Entregador: ";
+            // 
+            // cmbEntregador
+            // 
+            this.cmbEntregador.FormattingEnabled = true;
+            this.cmbEntregador.Location = new System.Drawing.Point(517, 238);
+            this.cmbEntregador.Name = "cmbEntregador";
+            this.cmbEntregador.Size = new System.Drawing.Size(133, 21);
+            this.cmbEntregador.TabIndex = 10;
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(389, 338);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(280, 21);
+            this.lbl4.TabIndex = 11;
+            this.lbl4.Text = "Cuantos artículos se entregan:";
+            // 
+            // lblCantidadArticulos
+            // 
+            this.lblCantidadArticulos.AutoSize = true;
+            this.lblCantidadArticulos.Location = new System.Drawing.Point(683, 344);
+            this.lblCantidadArticulos.Name = "lblCantidadArticulos";
+            this.lblCantidadArticulos.Size = new System.Drawing.Size(0, 13);
+            this.lblCantidadArticulos.TabIndex = 12;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(329, 398);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(526, 82);
+            this.btnConfirmar.TabIndex = 14;
+            this.btnConfirmar.Text = "CONFIRMAR";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            // 
+            // frmRecogida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1253, 492);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.lblCantidadArticulos);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.cmbEntregador);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnVolver);
+            this.Controls.Add(this.lblNumeroRecogida);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cmbEmpleado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmRecogida";
+            this.Text = "frmRecogida";
+            this.Load += new System.EventHandler(this.frmRecogida_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbEmpleado;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblNumeroRecogida;
+        private System.Windows.Forms.Button btnVolver;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbEntregador;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lblCantidadArticulos;
+        private System.Windows.Forms.Button btnConfirmar;
+    }
+}
