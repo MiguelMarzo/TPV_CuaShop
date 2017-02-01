df797fd29381b44e151b929b6d11860e4d27a0f3
Miguel <miguel-marzo@hotmail.es>
2017-02-01 12:04:42 +0100

    Created Venta class

---
+++ Entidades/Venta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Venta
+    {
+        private int numeroVenta;
+        private int numeroEmpleado;
+        private DateTime fechaVenta;
+        private double precio;
+    }
+}
