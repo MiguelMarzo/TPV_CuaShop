ae4142e1595300a3fb6a710a3b60df4a202b52fe
Miguel <miguel-marzo@hotmail.es>
2017-02-06 10:00:34 +0100

    Created frmInicio.cs

---
+++ CapaPresentacion/frmInicio.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace CapaPresentacion
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnAccionesEmpleado = new System.Windows.Forms.Button();
+            this.btnAdmin = new System.Windows.Forms.Button();
+            this.btnBuscarArticulo = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Rockwell", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(143, 20);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(584, 147);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "CuaShop";
+            // 
+            // btnAccionesEmpleado
+            // 
+            this.btnAccionesEmpleado.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAccionesEmpleado.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAccionesEmpleado.Location = new System.Drawing.Point(246, 170);
+            this.btnAccionesEmpleado.Name = "btnAccionesEmpleado";
+            this.btnAccionesEmpleado.Size = new System.Drawing.Size(340, 66);
+            this.btnAccionesEmpleado.TabIndex = 1;
+            this.btnAccionesEmpleado.Text = "AccionesEmpleado";
+            this.btnAccionesEmpleado.UseVisualStyleBackColor = true;
+            // 
+            // btnAdmin
+            // 
+            this.btnAdmin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAdmin.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdmin.Location = new System.Drawing.Point(246, 346);
+            this.btnAdmin.Name = "btnAdmin";
+            this.btnAdmin.Size = new System.Drawing.Size(340, 66);
+            this.btnAdmin.TabIndex = 2;
+            this.btnAdmin.Text = "Administración";
+            this.btnAdmin.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscarArticulo
+            // 
+            this.btnBuscarArticulo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnBuscarArticulo.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarArticulo.Location = new System.Drawing.Point(246, 258);
+            this.btnBuscarArticulo.Name = "btnBuscarArticulo";
+            this.btnBuscarArticulo.Size = new System.Drawing.Size(340, 66);
+            this.btnBuscarArticulo.TabIndex = 3;
+            this.btnBuscarArticulo.Text = "Buscar Articulo";
+            this.btnBuscarArticulo.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnSalir.Font = new System.Drawing.Font("Rockwell", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(830, 12);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(98, 60);
+            this.btnSalir.TabIndex = 4;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 489);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnBuscarArticulo);
+            this.Controls.Add(this.btnAdmin);
+            this.Controls.Add(this.btnAccionesEmpleado);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmInicio";
+            this.Text = "CuaShop";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frmInicio_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnAccionesEmpleado;
+        private System.Windows.Forms.Button btnAdmin;
+        private System.Windows.Forms.Button btnBuscarArticulo;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
