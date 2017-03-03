856abb444eb011e0a3fb4a17986b1fd6a738ecb5
Miguel <miguel-marzo@hotmail.es>
2017-03-03 12:13:47 +0100

    Admin form`s add employee method now copies the file to the source directory All about this is working, proper insert in database and correctl load of the picture The picture is displayed right as it is selected

--- CapaPresentacion/frmAdmin.cs
+++ CapaPresentacion/frmAdmin.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using CapaNegocio;
 using Entidades;
+using System.Text.RegularExpressions;
 
 namespace CapaPresentacion
 {
@@ -39,12 +40,8 @@ namespace CapaPresentacion
                     MessageBox.Show("Debes elegir un archivo con formato adecuado (.png, .jpeg, .jpg o .gif");
                     return;
                 }                
-            } 
-        }
-
-        private void grpAñadirEmpleado_Enter(object sender, EventArgs e)
-        {
-
+            }
+            pctBoxFotoEmpleado.Image = Image.FromFile(txtRutaFoto.Text);
         }
 
         private void btnSalir_Click(object sender, EventArgs e)
@@ -59,11 +56,16 @@ namespace CapaPresentacion
                 MessageBox.Show("Introduce un nombre de empleado");
                 return;
             } else
-            {                
-                String sourcePath = txtRutaFoto.Text;
-                String targetPath = "/";
-                String[] holis =  sourcePath.Split('/');
-                String fileName = holis[holis.Count() -1].ToString();                
+            {
+                String[] imagenSeleccionada = Regex.Split(txtRutaFoto.Text, @"(?<=[\\])");
+                String fileName = imagenSeleccionada[imagenSeleccionada.Count() - 1];
+                imagenSeleccionada.ToList().RemoveAt(imagenSeleccionada.Count() - 1);
+                String sourcePath = "";
+                for (int i = 0; i<imagenSeleccionada.Count() -1; i++)
+                {
+                    sourcePath += imagenSeleccionada[i];
+                }
+                String targetPath = ".";              
                 string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
                 string destFile = System.IO.Path.Combine(targetPath, fileName);            
  
@@ -73,7 +75,6 @@ namespace CapaPresentacion
                 }
                 //Copiar
                 System.IO.File.Copy(sourceFile, destFile, true);
-
                 String resultado = _negocio.InsertarEmpleado(txtNombreEmpleado.Text, fileName);
                 MessageBox.Show(resultado);
             }
